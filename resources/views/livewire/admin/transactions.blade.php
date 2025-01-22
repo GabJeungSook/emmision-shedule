7b95da061e87fd985e5e84a39a619cd09fd04cf3
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-01-22 16:23:35 +0800

    transactions

--- resources/views/livewire/admin/transactions.blade.php
+++ resources/views/livewire/admin/transactions.blade.php
@@ -1,3 +1,8 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <div class="mt-10 text-4xl font-bold mb-4">
+        Transactions
+    </div>
+    <div class="p-4 mt-5">
+        {{ $this->table }}
+    </div>
 </div>
