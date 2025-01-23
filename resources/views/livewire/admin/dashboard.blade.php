b3f3f973cb968fc4cb700b625b2d8ce239ba6514
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-01-23 11:11:26 +0800

    update

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -12,11 +12,11 @@
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
             <dt class="truncate text-sm font-medium text-gray-500">Total Users</dt>
-            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{$transactions->count()}}</dd>
+            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{$users->where('role', 'user')->count()}}</dd>
           </div>
           <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
-            <dt class="truncate text-sm font-medium text-gray-500">Total Transctions</dt>
-            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{$users->count()}}</dd>
+            <dt class="truncate text-sm font-medium text-gray-500">Total Transactions</dt>
+            <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{{$transactions->count()}}</dd>
           </div>
           <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
             <dt class="truncate text-sm font-medium text-gray-500">Total Schedules</dt>
